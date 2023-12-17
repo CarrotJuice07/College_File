9b98d0d881f8fc8d7800282f79b0ec220c563fba
CarrotJuice07 <kimpet0717@naver.com>
2023-12-18 02:51:06 +0900

    미디어아트 시험보기 싫다

---
+++ 건축환경계획/과제/과제 2/분석.sol
@@ -0,0 +1,51 @@
+#
+# The Solar Tool v2.00
+# © Dr.A.J.Marsh '92-'06.
+#
+
+
+# Location.
+Latitude: -0.559
+Longitude: 2.025
+TimeZone: 2.356
+Orientation: -55.000
+TiltAngle: 0.000
+Date: 202  21st July
+Time: 14.008333
+
+# Results.
+Solar_Azimuth: 6.39°
+Solar_Altitude: 37.08°
+Horizontal_Shadow_Angle: 61.39°
+Vertical_Shadow_Angle: 57.64°
+
+# 3D Model.
+WallWidth: 5000.0
+WallHeight: 3200.0
+WallDepth: 270.0
+WindowSill: 100.0
+WindowWidth: 3500.0
+WindowHeight: 3000.0
+WindowSide: 100.0
+WindowTop: 100.0
+Snap: 50.0
+
+# Shades.
+Shade: 0 0 1 10.0 0.0 90.0 0.0 0.0 0.0
+Shade: 1 1 0 0.0 900.0 0.0 100.0 50.0 0.0
+Shade: 2 2 0 250.0 250.0 -35.0 500.0 500.0 0.0
+Shade: 3 3 1 0.0 3200.0 90.0 50.0 270.0 0.0
+Shade: 4 4 1 450.0 3200.0 -90.0 270.0 50.0 0.0
+Shade: 5 5 1 450.0 3200.0 90.0 50.0 270.0 0.0
+Shade: 6 6 1 900.0 3200.0 -90.0 270.0 50.0 0.0
+Shade: 7 7 1 900.0 3200.0 90.0 50.0 270.0 0.0
+Shade: 8 8 1 1350.0 3200.0 -90.0 270.0 50.0 0.0
+Shade: 9 9 1 1350.0 3200.0 90.0 50.0 270.0 0.0
+Shade: 10 3 0 2000.0 2000.0 0.0 1000.0 1000.0 0.0
+
+# View.
+SunPath: 6 1 1 2
+OpenGLSettings: 41 65 1 2 1 1.00 0.50 0.50
+OpenGLDimensions: 50.000 1000.000 50.000 1000.000 0.008 0.000
+OpenGLView: 63.172 1.452 0.601
+OpenGLColours: 0 FFFFFF
